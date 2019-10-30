1fc706e6bab2aa0bc4af1a259c9e604bfdf1e6a4
alanb128 <39385685+alanb128@users.noreply.github.com>
2019-10-30 11:27:39 -0400

    Add files via upload
    Initial upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Installing Adafruit code and data in /boot..."
+cd /tmp
+curl -LO https://github.com/adafruit/Pi_Eyes/archive/master.zip
+unzip master.zip
+# Moving between filesystems requires copy-and-delete:
+cp -r Pi_Eyes-master /boot/Pi_Eyes
+rm -rf master.zip Pi_Eyes-master
+echo "Installing gpio-halt in /usr/local/bin..."
+curl -LO https://github.com/adafruit/Adafruit-GPIO-Halt/archive/master.zip
+unzip master.zip
+cd Adafruit-GPIO-Halt-master
+make
+mv gpio-halt /usr/local/bin
+cd ..
+rm -rf Adafruit-GPIO-Halt-master
